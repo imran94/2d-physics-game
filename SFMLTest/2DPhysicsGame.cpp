db4d198f31818e4a0019f674d6325dd1ccba7186
imran94 <iraja094@gmail.com>
2017-01-02 22:39:52 +0800

    Set project settings to successfully build the Box2D library

--- SFMLTest/2DPhysicsGame.cpp
+++ SFMLTest/2DPhysicsGame.cpp
@@ -2,10 +2,20 @@
 //
 #include "stdafx.h"
 #include <SFML\Graphics.hpp>
+#include <Box2D/Box2D.h>
 
 
 int main()
 {
+	//Construct a b2World
+	b2Vec2 gravity(0.0f, -10.0f);
+	b2World world(gravity);
+
+	//Request b2World's body factory to construct a b2Body
+	b2BodyDef groundBodyDef;
+	groundBodyDef.position.Set(0.0f, -10.0f);
+	b2Body* groundBody = world.CreateBody(&groundBodyDef);
+
 	sf::RenderWindow window(sf::VideoMode(500, 300), "YPWong is a nigger");
 
 	sf::CircleShape shape(100.f);
