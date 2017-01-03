4aaf90121f924d6453651a55794ea0feb11b7456
blackmetall95 <rzcc5911@yahoo.co.uk>
2017-01-03 17:17:19 +0800

    Added a Floor

--- SFMLTest/2DPhysicsGame.cpp
+++ SFMLTest/2DPhysicsGame.cpp
@@ -8,26 +8,30 @@ using namespace std;
 
 const float SCALE = 5.f;
 
+void CreateGround(b2World& world, float X, float Y);
 void CreateBox(b2World& world, int MouseX, int MouseY);
 
 int main()
 {
-
 	//Construct a b2World
 	b2Vec2 gravity(0.0f, 9.8f);
 	b2World world(gravity);
+	CreateGround(world, 400.f, 500.f);
 
 	//Request b2World's body factory to construct a b2Body
 	b2BodyDef groundBodyDef;
 	groundBodyDef.position.Set(0.0f, -10.0f);
 	b2Body* groundBody = world.CreateBody(&groundBodyDef);
 
-	sf::RenderWindow window(sf::VideoMode(800, 600, 32), "YPWong is a nigger");
+	sf::RenderWindow window(sf::VideoMode(1280, 720, 32), "YPWong is a nigger");
 	window.setFramerateLimit(60);
 	
 	//Load Texture
+	sf::Texture groundTex;
 	sf::Texture boxTex;
+	groundTex.loadFromFile("Assets\\Texture\\groundtexture.bmp");
 	boxTex.loadFromFile("Assets\\Texture\\boxtexture2.bmp");
+
 
 	/*sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);*/
@@ -62,7 +66,15 @@ int main()
 				window.draw(sprite);
 				cout << "Sprite Drawn\n";
 				++BodyCount;
-				//break;
+			}
+			else
+			{
+				sf::Sprite groundSprite;
+				groundSprite.setTexture(groundTex);
+				groundSprite.setOrigin(350.f, 8.f);
+				groundSprite.setPosition(BodyIterator->GetPosition().x*SCALE, BodyIterator->GetPosition().y*SCALE);
+				groundSprite.setRotation(180 / b2_pi*BodyIterator->GetAngle());
+				window.draw(groundSprite);
 			}
 		}
 
@@ -78,6 +90,21 @@ int main()
 		window.display();
 	}
     return 0;
+}
+
+void CreateGround(b2World& world, float X, float Y)
+{
+	b2BodyDef BodyDef;
+	BodyDef.position = b2Vec2(X / 3.f, Y / 5.f);
+	BodyDef.type = b2_staticBody;
+	b2Body* Body = world.CreateBody(&BodyDef);
+
+	b2PolygonShape Shape;
+	Shape.SetAsBox((700.f / 2) / SCALE, (16.f / 2) / SCALE);
+	b2FixtureDef fixDef;
+	fixDef.density = 0.f;
+	fixDef.shape = &Shape;
+	Body->CreateFixture(&fixDef);
 }
 
 void CreateBox(b2World& world, int MouseX, int MouseY)
